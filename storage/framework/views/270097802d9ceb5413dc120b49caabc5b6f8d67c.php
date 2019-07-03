9618be5e03feaa5e9262d8478af98d1824b78aaa
dinescu-damian <52448238+dinescu-damian@users.noreply.github.com>
2019-07-04 00:13:54 +0300

    bg pattern

--- storage/framework/views/270097802d9ceb5413dc120b49caabc5b6f8d67c.php
+++ storage/framework/views/270097802d9ceb5413dc120b49caabc5b6f8d67c.php
@@ -8,7 +8,7 @@
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
-<body>
+<body background="<?php echo e(asset('/img/bg.png')); ?>">
 
 
 <nav class="navbar navbar-expand-lg navbar-light bg-orange" style="background-color: #FF851B">
